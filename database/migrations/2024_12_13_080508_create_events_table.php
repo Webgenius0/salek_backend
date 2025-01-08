8df0a02e56f1298735eed0f830b1f30b62ccbd2b
isratahamedsabbir <isratahanedsabbir@gmail.com>
2025-01-08 16:45:57 +0600

    update

--- database/migrations/2024_12_13_080508_create_events_table.php
+++ database/migrations/2024_12_13_080508_create_events_table.php
@@ -24,6 +24,7 @@ return new class extends Migration
             $table->string('thumbnail', 250)->nullable();
             $table->decimal('latitude', 10, 8)->nullable();
             $table->decimal('longitude', 11, 8)->nullable();
+            $table->longText('address')->nullable();
             $table->foreignId('created_by')->constrained('users')->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('updated_by')->nullable()->constrained('users')->onUpdate('cascade')->onDelete('cascade');
             $table->enum('status', ['on_going', 'upcoming', 'complete'])->default('upcoming');
