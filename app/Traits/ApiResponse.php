7f99d3b02062c11434f857d20ce1ffb68cce57aa
shamim192 <shamim.cse.gub@gmail.com>
2025-01-14 15:16:11 +0600

    mark as complete

--- app/Traits/ApiResponse.php
+++ app/Traits/ApiResponse.php
@@ -53,4 +53,24 @@ trait ApiResponse
             'code'    => $code
         ], $code);
     }
+
+    public function success($data, $message = null, $code = 200)
+    {
+        return response()->json([
+            'success' => true,
+            'message' => $message,
+            'data' => $data,
+            'code' => $code
+        ], $code);
+    }
+
+    public function error($data, $message = null, $code = 404)
+    {
+        return response()->json([
+            'status' => false,
+            'message' => $message,
+            'data' => $data,
+            'code' => $code
+        ], $code);
+    }
 }
