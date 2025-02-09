4206121b36b541556441e79d4365a57c3a726705
shamim192 <shamim.cse.gub@gmail.com>
2025-02-09 12:32:02 +0600

    update

--- routes/api/api.php
+++ routes/api/api.php
@@ -102,8 +102,8 @@ Route::middleware(['auth:api'])->group(function () {
         Route::prefix('course')->name('course.')->group(function () {
             Route::get('/all', 'index')->name('list');
             Route::post('/details/{id}', 'show')->name('details');
-            
-            Route::get('/course/{courseId}/chapters-lessons', 'getCourseChaptersWithLessons');
+
+            Route::get('/{courseId}/chapters-lessons', 'getCourseChaptersWithLessons');
 
             Route::get('/level/{id}', 'level')->name('level');
             // Route::get('/popular', 'popularCourse')->name('popular');
